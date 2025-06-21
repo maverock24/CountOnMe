4862f7d8809ae0ab1bd905d6d1c8090e439c1c82
John Doe <johndoe@example.com>
2025-06-22 01:07:52 +0300

    feat: update ListTile layout for improved structure and spacing

--- components/ListTile.tsx
+++ components/ListTile.tsx
@@ -62,10 +62,10 @@ const ListTile = ({
         colors={['rgb(49, 67, 77)', 'rgb(38, 48, 54)', 'rgb(28, 37, 43)']}
       >
       <>
-  <View style={{ flexDirection: 'row', alignItems: 'flex-start', width: '100%'}}>
+  <View style={{ flexDirection: 'column', alignItems: 'flex-start', width: '100%'}}>
 
     {/* Info row: mock values for calories and training level, now below the title */}
-    <View style={{ flexDirection: 'column', alignItems: 'flex-start', width: 90, borderRightColor: '#b0e0e6', borderRightWidth: 1 }}>
+    <View style={{ flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-between', borderBottomColor: '#b0e0e6', borderBottomWidth: 1, width: '100%', paddingBottom: 5 }}>
       <Text style={commonStyles.listItemTitle}>{title}</Text>
       <Text style={{ fontSize: 12, color: '#b0e0e6', marginBottom: 5 }}>Calories: 120</Text>
       <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 2 }}>
@@ -82,7 +82,7 @@ const ListTile = ({
       </View>
     </View>
   
-  <View style={{ flexDirection: 'row', width: '80%', paddingHorizontal: 15, marginTop: 20 }}>
+  <View style={{ flexDirection: 'row', width: '100%', paddingHorizontal: 15, marginTop: 10 }}>
     {value &&
     value.split(';').map((time, index) => (
       <Text key={index} style={isSelected && index === workoutStage ? commonStyles.listItemValueText : commonStyles.listItemValue}>
